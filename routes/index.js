f587bb80406982402839d45564c7ff64bbb042df
winterlood <king199777@gmail.com>
2023-02-27 14:56:12 +0900

    [Feat] add random quote api feature

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const quotes = require("./../data/quote.json");
+const express = require("express");
+const router = express.Router();
+
+function getRandomQuote() {
+  let quote;
+  while (!quote) {
+    const idx = Math.floor(Math.random() * 594);
+    quote = quotes.at(idx);
+  }
+  return `${quote.message} - ${quote.author}`;
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  const quote = getRandomQuote();
+  res.json([
+    {
+      result: "success",
+    },
+    {
+      respond: quote,
+    },
+  ]);
+  // res.render("index", { title: "Expre2ss2" });
 });
 
 module.exports = router;
