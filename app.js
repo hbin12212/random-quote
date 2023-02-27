03184d79df99e67dd2cab398cbc470acdbdab7b5
winterlood <king199777@gmail.com>
2023-02-27 15:23:30 +0900

    [Chore] move cors require line

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
-const cors = require("cors");
 var app = express();
 
 app.use(cors());
