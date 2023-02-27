ca57e1d453c356ab9696a3181059173b74fcdf4f
winterlood <king199777@gmail.com>
2023-02-27 15:05:56 +0900

    [Feat] Add cors

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-
+const cors = require("cors");
 var app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
